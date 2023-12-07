955e7b765dbe5a5b38dddd798424845ad26f9c60
Venkatesh Narayanan <venknar@users.noreply.github.com>
2023-12-07 14:13:07 +0000

    Cargo sample

---
+++ Samples/cargo-hello-world/target/debug/cargo-hello-world.d
@@ -0,0 +1,1 @@
+/workspaces/RustSamples/Samples/cargo-hello-world/target/debug/cargo-hello-world: /workspaces/RustSamples/Samples/cargo-hello-world/src/main.rs
---
+++ Samples/cargo-hello-world/target/debug/deps/cargo_hello_world-405a109012747b61.d
@@ -0,0 +1,5 @@
+/workspaces/RustSamples/Samples/cargo-hello-world/target/debug/deps/cargo_hello_world-405a109012747b61: src/main.rs
+
+/workspaces/RustSamples/Samples/cargo-hello-world/target/debug/deps/cargo_hello_world-405a109012747b61.d: src/main.rs
+
+src/main.rs:
